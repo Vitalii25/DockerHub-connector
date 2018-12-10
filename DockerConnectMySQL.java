d4e2af7db1d8d8fe03e31d4aecb2e66cc826da39
Vitalii25 <Vitaliiuapl@gmail.com>
2018-12-10 22:43:26 +0100

    Update DockerConnectMySQL.java

--- DockerConnectMySQL.java
+++ DockerConnectMySQL.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class DockerConnectMySQL {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/database";
-    static final String USER = "VMayuk";
+    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/bazadanych";
+    static final String USER = "vmayuk";
     static final String PASS = "password";
 
     public static void main(String[] args) {
@@ -17,72 +17,82 @@ public class DockerConnectMySQL {
                     conn = DriverManager.getConnection(DB_URL, USER, PASS);
                     connect = true;
                 } catch (Exception e) {
-                    System.out.println("Connecting with database server");
-                    Thread.sleep(1000);
+                    System.out.println("Connecting...");
+                    Thread.sleep(1200);
                 }
+		   
             } while (!connect);
-			System.out.println("Connected with database server");
+			System.out.println("Connected");
             stmt = conn.createStatement();
             String sql;
-            sql = "DROP TABLE IF EXISTS Students";
+
+            sql = "DROP TABLE IF EXISTS People";
             stmt.executeUpdate(sql);
-            sql = "CREATE TABLE Students (StudentId int, FirstName varchar(255), LastName varchar(255));";
+		//tworzy tabelę
+            sql = "CREATE TABLE People (Id int, Name varchar(255), Surname varchar(255), Age int );";
             stmt.executeUpdate(sql);
-            sql = "INSERT INTO Students(StudentId, FirstName, LastName) VALUES (1,'Vitalii', 'Mayuk'),(2, 'Nick', 'Nickolaskin'),(3, 'John', 'Jonnatan');";
+		//dodaje do tabeli dane
+            sql = "INSERT INTO People(Id, Name, Surname, Age) VALUES (1,'Vitalii', 'Mayuk', '21'),(2, 'One', 'More', '34'),(3, 'Second', 'One', '16');";
             stmt.executeUpdate(sql);
             Scanner menu = new Scanner(System.in);
             String i;
             do {
-                System.out.println("");
-                System.out.println("SQL Menu");
-                System.out.println("Type number");
-                System.out.println("Choose an option:");
-                System.out.println("(1) adding");
-                System.out.println("(2) display");
-                System.out.println("(3) exit");
+                System.out.println(""); 
+                System.out.println("Choose option");
+                System.out.println("[1] add");
+                System.out.println("[2] display values");
+                System.out.println("[3] exit");
                 i = (String) menu.next();
                 switch (i) {
-                    case "1": {
+                    case "1": {	  
+			    //dodanie danych do tabeli
                         Scanner insert = new Scanner(System.in);
-                        sql = "SELECT StudentId FROM Student ORDER BY StudentId DESC LIMIT 1;";
+                        sql = "SELECT Id FROM People ORDER BY Id DESC LIMIT 1;";
                         ResultSet rs = stmt.executeQuery(sql);
                         int e = 0;
                         if (rs.next()) {
-                            e = rs.getInt("StudentId");
+                            e = rs.getInt("Id");
                         }
                         rs.close();
                         e++;
-                     sql = "INSERT INTO Students (StudentId, FirstName, LastName) VALUES (" + e + ",'";
-                        System.out.println("Type FirstName:");
+                        sql = "INSERT INTO People (Id, Name, Surname, Age) VALUES (" + e + ",'";
+                        System.out.println("Type name:");
                         sql += insert.nextLine();
                         sql += "', '";
-                        System.out.println("Type LastName:");
+                        System.out.println("Type surname:");
+                        sql += insert.nextLine();
+                        sql += "', '";
+                        System.out.println("Type age:");
                         sql += insert.nextLine();
                         sql += "');";
                         stmt.executeUpdate(sql);
-						System.out.println("Work");
+						System.out.println("added to table[!]");
                         break;
                     }
                     case "2": {
-                        sql = "SELECT StudentId, FirstName, LastName FROM Students";
+			    
+                        sql = "SELECT Id, Name, Surname, Age FROM People";
                         ResultSet rs = stmt.executeQuery(sql);
-                     System.out.printf("|%5s|%15s|%15s|\n", "StudentId: ", "FirstName: ", "LastName: ");
+                        System.out.printf("|%5s|%15s|%15s|%15s|\n", "Id: ", "Name: ", "Surname: ", "Age: ");
                         while (rs.next()) {
-                            int StudentId = rs.getInt("StudentId");
-                            String FirstName = rs.getString("FirstName");
-                            String LastName = rs.getString("LastName");
-                            System.out.printf("|%4d |%14s |%14s |\n", StudentId, FirstName, LastName);
+                            int id = rs.getInt("Id");
+                            String name = rs.getString("Name");
+                            String surname = rs.getString("Surname");
+                            String age = rs.getString("Age");
+                            System.out.printf("|%4d |%14s |%14s |%14s |\n", id, name, surname, age);
                         }
                         rs.close();
                         break;
                     }
                     case "3": {
+		
                         i = "3";
                         break;
                     }
                     default: {
+			
 						System.out.println("");
-                        System.out.println("One more time, you shoud win ");
+                        System.out.println("Choose a right option[!]");
                         break;
                     }
                 }
